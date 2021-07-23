7f796abcd1010878e6c9f44d3c3e58f0fc632c0b
surendranj <Jishnu.Surendran@dowjones.com>
2021-07-23 15:18:06 +0530

    bootsrap and lodash import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { random } from 'lodash';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  number = 0;
   title = 'my-project';
+  onClick(){
+    this.number = random(1,10);
+  }
 }
